19ab85120127fb02a06dbe7da45a34d8ab02b779
Dexter9808 <akash.work16@gmail.com>
2024-05-18 11:35:34 +0000

    variables.js

---
+++ Demo1/Variable.js
@@ -0,0 +1,13 @@
+// constant variable value can not be changed through out the program.
+const accountNO = 1445
+//Variable : whose value can be changed through out the program.Scope for 'VAR' will be global 
+var Name2 = "Akash Patil"
+
+//let var :  whose value can be changed through out the program.Scope for 'let' will be defined where you have initailized it.
+let Name = "Akash"
+Name3 = "Akash P Patil"
+
+console.log(accountNO);
+
+console.table([accountNO,Name,Name2,Name3])
+
